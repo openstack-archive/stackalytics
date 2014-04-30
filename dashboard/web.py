850716be32c504684282220ea72d7b8ba2e8ff7a
pkholkin <pkholkin@mirantis.com>
2014-04-30 14:52:43 +0400

    Added "new_companies" list for the period
    implements bp member-directory
    
    Change-Id: I7e00388a7f9b1bcb4343be34738dc8ab74932f62

--- dashboard/web.py
+++ dashboard/web.py
@@ -94,6 +94,35 @@ def _get_aggregated_stats(records, metric_filter, keys, param_id,
     return response
 
 
+@app.route('/api/1.0/new_companies')
+@decorators.jsonify('stats')
+@decorators.exception_handler()
+@decorators.record_filter(ignore='start_date')
+def get_new_companies(records):
+
+    days = int(flask.request.args.get('days') or reports.DEFAULT_DAYS_COUNT)
+    start_date = int(time.time()) - days * 24 * 60 * 60
+
+    result = {}
+    for record in records:
+        company_name = record['company_name']
+        date = record['date']
+
+        if company_name not in result or result[company_name] > date:
+            result[company_name] = date
+
+    response = list(({'name': company_name,
+                      'date': result[company_name],
+                      'date_str': helpers.format_date(result[company_name])})
+                    for company_name in result
+                    if result[company_name] >= start_date)
+
+    response.sort(key=lambda x: x['date'], reverse=True)
+    utils.add_index(response)
+
+    return response
+
+
 @app.route('/api/1.0/stats/companies')
 @decorators.jsonify('stats')
 @decorators.exception_handler()
@@ -320,6 +349,7 @@ def get_module(module):
 def get_members(records):
     response = []
     for record in records:
+        record = vault.extend_record(record)
         nr = dict([(k, record[k]) for k in
                    ['author_name', 'date', 'company_name', 'member_uri']])
         nr['date_str'] = helpers.format_date(nr['date'])
